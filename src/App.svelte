c4b1d0c9aa90a59f37be2c0718b6a49b658b9976
Bishwajeet Parhi <bishwajeet.techmaster@gmail.com>
2022-06-23 00:54:27 +0530

    initial commit + navbar

---
+++ src/App.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import Header from "./lib/Header.svelte";
+</script>
+
+<svelte:head>
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+    rel="stylesheet"
+  />
+</svelte:head>
+
+<main>
+  <Header />
+</main>
+
+<style>
+  :root {
+    background-color: #F8F8F8;
+  }
+  
+  main {
+    text-align: center;
+    padding: 1em;
+    margin: 0 auto;
+    font-family: "Rubik", sans-serif;
+  }
+</style>
---
+++ src/lib/Header.svelte
@@ -0,0 +1,112 @@
+<script lang="ts">
+  import logo from "../assets/logo.png";
+</script>
+
+<nav class="flex justify-between items-center">
+  <div class="w-1/2">
+    <a href="/" class="logo">
+      <img
+      class = "h-14 w-auto"
+        src= {logo}
+        alt="Keep"
+      />
+    </a>
+  </div>
+  <div class="absolute  right-4 flex items-center sm:hidden">
+    <button
+      type="button"
+      class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+      aria-controls="mobile-menu"
+      aria-expanded="false"
+    >
+      <span class="sr-only">Open main menu</span>
+      <!--
+            Icon when menu is closed.
+
+            Heroicon name: outline/menu
+
+            Menu open: "hidden", Menu closed: "block"
+          -->
+      <svg
+        class="block h-6 w-6"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="2"
+        stroke="currentColor"
+        aria-hidden="true"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M4 6h16M4 12h16M4 18h16"
+        />
+      </svg>
+      <!--
+            Icon when menu is open.
+
+            Heroicon name: outline/x
+
+            Menu open: "block", Menu closed: "hidden"
+          -->
+      <svg
+        class="hidden h-6 w-6"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="2"
+        stroke="currentColor"
+        aria-hidden="true"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M6 18L18 6M6 6l12 12"
+        />
+      </svg>
+    </button>
+  </div>
+
+  <div class="hidden sm:block">
+    <ul class="flex w-5/6 justify-evenly items-center ">
+      <li class="mr-6">
+        <!-- svelte-ignore a11y-invalid-attribute -->
+        <a href="#">Home</a>
+      </li>
+      <li class="mr-6">
+        <!-- svelte-ignore a11y-invalid-attribute -->
+        <a href="#">Services</a>
+      </li>
+      <li class="mr-6">
+        <!-- svelte-ignore a11y-invalid-attribute -->
+        <a href="#">Skills</a>
+      </li>
+      <li class="mr-6">
+        <!-- svelte-ignore a11y-invalid-attribute -->
+        <a href="#">Experience</a>
+      </li>
+      <li class="mr-6">
+        <!-- svelte-ignore a11y-invalid-attribute -->
+        <a href="#">Contact</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+<!-- <style>
+  nav {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 1em;
+    margin: 0 auto;
+  }
+  nav ul {
+    display: flex;
+    justify-content: space-evenly;
+    flex: 1;
+    align-items: center;
+    padding: 1em;
+    margin: 0 auto;
+  }
+</style> -->
