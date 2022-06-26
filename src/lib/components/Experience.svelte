509691b2503fd02e286d093dafee1a82bc29fa3c
Bishwajeet Parhi <bishwajeet.techmaster@gmail.com>
2022-06-27 01:53:49 +0530

    fix undefined

--- src/lib/components/Experience.svelte
+++ src/lib/components/Experience.svelte
@@ -4,6 +4,6 @@
   
   
   <div class = "items-start text-start">
-    <p class = "text-xl font-medium mb-4">{experience.title} - {experience.Presence} ({experience.date_joining} - {experience.date_left}) </p>
+    <p class = "text-xl font-medium mb-4">{experience.title} - {experience.company} ({experience.date_joining} - {experience.date_left}) </p>
     <p>{experience.description}</p>
    </div>
