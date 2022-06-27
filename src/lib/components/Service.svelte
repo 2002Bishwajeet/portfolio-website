9bb36523c026fc58ed289f186b1506b9f438b6a6
Bishwajeet Parhi <bishwajeet.techmaster@gmail.com>
2022-06-27 20:51:19 +0530

    lint errors fixed

--- src/lib/components/Service.svelte
+++ src/lib/components/Service.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
    import Icon from "@iconify/svelte";
-  export let icon;
-  export let title;
-  export let description;
+  export let icon:string;
+  export let title:string;
+  export let description:string;
 </script>
 
 <div class=" min-w-min content-start items-start text-start w-96 mt-16">
